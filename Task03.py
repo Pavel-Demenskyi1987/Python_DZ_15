a133fcf3e7f479a1315341a16e854575acc5926b
Pavel <shadow-hunter2000@yandex.ru>
2024-10-22 15:28:14 +0800

    Домашняя работа

---
+++ Task03.py
@@ -0,0 +1,19 @@
+"""Задача 3. Планирование задач
+Напишите функцию, которая принимает количество дней от текущей даты и
+возвращает дату, которая наступит через указанное количество дней. Дополнительно,
+выведите эту дату в формате YYYY-MM-DD.
+"""
+
+from datetime import datetime, timedelta
+
+
+def future_date(days_from_now):
+    today = datetime.now()
+    future_date = today + timedelta(days=days_from_now)
+    formatted_future_date = future_date.strftime('%Y-%m-%d')
+    return formatted_future_date
+
+
+if __name__ == '__main__':
+    days = 30
+    print(f'Date {days} days from now: {future_date(days)}')
---
+++ Task_02.py
@@ -0,0 +1,20 @@
+"""Задача 2. Работа с текущим временем и датой
+Напишите скрипт, который получает текущее время и дату, а затем выводит их в
+формате YYYY-MM-DD HH:MM:SS. Дополнительно, выведите день недели и номер
+недели в году"""
+
+from datetime import datetime
+
+
+def display_current_datetime():
+    now = datetime.now()
+    formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+    day_of_week = now.strftime('%A')
+    week_number = now.isocalendar()[1]
+    print(f'Current date and time: {formatted_date}')
+    print(f'Day of the week: {day_of_week}')
+    print(f'Week number: {week_number}')
+
+
+if __name__ == '__main__':
+    display_current_datetime()
